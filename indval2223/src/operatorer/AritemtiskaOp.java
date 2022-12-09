19c5f9d4d0db243999d87a704125372ebf97676c
daande01 <daniel.evert.andersson@gmail.com>
2022-12-09 09:30:37 +0100

    new files

---
+++ indval2223/src/operatorer/AritemtiskaOp.java
@@ -0,0 +1,28 @@
+package operatorer;
+
+public class AritemtiskaOp {
+	public static void main(String[] args) {
+		
+		int i=2;
+		
+		i++;
+		
+		i--;
+		
+		i = 2+3;
+		i+=3;
+		//______________________________
+		i=2-3;
+		i-=3;
+		i*=3;		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+}
---
+++ indval2223/src/upp/Upp44.java
@@ -0,0 +1,21 @@
+package upp;
+
+public class Upp44 {
+
+	public static void main(String[] args) {
+		// obs inte komplett lösning men ett steg påvägen
+		
+		double d = 140;
+		
+		double d2= d/100; 
+		
+		 int i =(int)(d2+0.5);
+		
+		 double d3= i*100;
+		System.out.println(d3);
+		
+	}
+	
+	
+	
+}
