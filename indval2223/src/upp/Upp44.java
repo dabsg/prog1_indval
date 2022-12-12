0783702ae9c9715baeb854b9879490df76f2e4db
daande01 <daniel.evert.andersson@gmail.com>
2022-12-12 08:41:53 +0100

    new file

--- indval2223/src/upp/Upp44.java
+++ indval2223/src/upp/Upp44.java
@@ -4,18 +4,16 @@ public class Upp44 {
 
 	public static void main(String[] args) {
 		// obs inte komplett lösning men ett steg påvägen
-		
+
 		double d = 160;
-		
-		double d2= d/100;   // d2 =1.6
-		
-		 int i =(int)(d2+0.5);  //  1.6+0.5=2.1 kapa dec ger 2
-		
-		 double d3= i*100;  // gångrar 2*100 = 200
+
+		double d2 = d / 100; // d2 =1.6
+
+		int i = (int) (d2 + 0.5); // 1.6+0.5=2.1 kapa dec ger 2
+
+		double d3 = i * 100; // gångrar 2*100 = 200
 		System.out.println(d3);
-		
+
 	}
-	
-	
-	
+
 }
