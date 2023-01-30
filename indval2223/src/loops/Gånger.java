3f6604eaa96888e109d993e88a6a6d94cdf8945c
daande01 <daniel.evert.andersson@gmail.com>
2023-01-30 09:38:31 +0100

    new

---
+++ indval2223/src/loops/Gånger.java
@@ -0,0 +1,17 @@
+package loops;
+
+public class Gånger {
+	public static void main(String[] args) {
+
+		for(int k=0;k<10;k++) {
+			
+			for (int i = 0; i < 10; i++) {
+				System.out.println(k+"*"+i+"="+(k*i));
+				
+			}
+		System.out.println();
+		}
+		
+		
+	}
+}
