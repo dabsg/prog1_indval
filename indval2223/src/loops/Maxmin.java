7a6c0a58dad946dc7fc446c54eaadeeca04924f5
daande01 <daniel.evert.andersson@gmail.com>
2023-01-31 14:04:34 +0100

    new

---
+++ indval2223/src/loops/Maxmin.java
@@ -0,0 +1,22 @@
+package loops;
+
+public class Maxmin {
+	public static void main(String[] args) {
+
+		int arr[]= {2,4,6,1,6,8,2,99,44};
+		int max=Integer.MIN_VALUE;
+		
+		
+		
+	for (int i = 0; i < arr.length; i++) {
+		
+		
+		if(arr[i]> max ){
+			
+			max=arr[i];
+		}
+		
+	}	
+		
+	}
+}
