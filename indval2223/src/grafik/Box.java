833460f056d270bf9ce4454ad7e432d7393a5b9c
daande01 <daniel.evert.andersson@gmail.com>
2023-04-03 08:43:34 +0200

    new file

---
+++ indval2223/src/grafik/Box.java
@@ -0,0 +1,51 @@
+package grafik;
+
+
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+
+public class Box {
+
+	JFrame f = new JFrame();
+	JCheckBox b = new JCheckBox();
+	JButton knapp = new JButton("skapa");
+	
+	
+	
+	public Box() {
+		
+		f.setVisible(true);
+		f.setLayout(new FlowLayout());
+		f.setSize(500,500);
+		f.add(b);
+		f.add(knapp);
+		
+		knapp.addActionListener(e->{
+			
+			if(b.isSelected()) {
+				
+				System.out.println("i bockad");
+				
+			}else {
+				
+				System.out.println("inte i bockad");
+			}
+			
+		});
+		
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		new Box();
+		
+	}
+	
+	
+}
