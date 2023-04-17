81f7e6bb37d56849b98b2713ad89fd91914aaccb
daande01 <daniel.evert.andersson@gmail.com>
2023-04-17 09:30:21 +0200

    new file

--- indval2223/src/egengrafik/Boll1.java
+++ indval2223/src/egengrafik/Boll1.java
@@ -1,25 +1,50 @@
 package egengrafik;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.Timer;
 
 
 public class Boll1 extends JComponent {
 
+	int x=0;
+	int y=0;
+	Timer t;
+	
 	public Boll1() {
 		
 		this.setPreferredSize(new Dimension(400,400));
 		
+		t= new Timer(50, e->{
+			
+			
+		update();	
+		repaint();	
+		});
+		
+		t.start();
 	}
+	
+	public void update() {
+		
+		x++;
+		y++;
+		System.out.println("hej");
+	}
+	
+	
+	
 	
 	@Override
 	protected void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
+		g.setColor(Color.RED);
+		g.fillOval(x, y, 30, 30);
 		
-		g.fillOval(100, 100, 30, 30);
 		
 		super.paintComponent(g);
 	}
@@ -34,6 +59,7 @@ public class Boll1 extends JComponent {
 		
 		f.setContentPane(b);
 		f.setVisible(true);
+		f.pack();
 		
 		
 		
