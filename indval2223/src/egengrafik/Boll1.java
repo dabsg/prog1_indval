d6667d7b3baa5bc0db392c82eb905855b27b9055
daande01 <daniel.evert.andersson@gmail.com>
2023-04-21 08:51:19 +0200

    boll

--- indval2223/src/egengrafik/Boll1.java
+++ indval2223/src/egengrafik/Boll1.java
@@ -8,62 +8,65 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
-
 public class Boll1 extends JComponent {
 
-	int x=0;
-	int y=0;
+	// instans variabler för position på skärmen samt timer behållare
+	int x = 200;
+	int y = 200;
+	int xv=3;
+	int yv=2;
 	Timer t;
-	
+
+	// konstruktor ,körs direkt när man skapar objekt av klassen
 	public Boll1() {
-		
-		this.setPreferredSize(new Dimension(400,400));
-		
-		t= new Timer(50, e->{
-			
-			
-		update();	
-		repaint();	
+
+		this.setPreferredSize(new Dimension(400, 400));
+
+		//timer skapas andropar update metoden samt repaint var 50 ms
+		t = new Timer(30, e -> {
+
+			update();
+			repaint();
 		});
-		
+
+		// startar timer
 		t.start();
 	}
+
+	// förändrar x och y 
 	
 	public void update() {
+
+		x= x+xv;
+		y=y+yv;
 		
-		x++;
-		y++;
-		System.out.println("hej");
+		if(x>(400-30 )) {
+			
+			xv= xv*-1;
+			
+		}
+		
+		
 	}
-	
-	
-	
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
 		// TODO Auto-generated method stub
 		g.setColor(Color.RED);
 		g.fillOval(x, y, 30, 30);
-		
-		
+
 		super.paintComponent(g);
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
-		
-		
-		JFrame f= new JFrame();
-		Boll1 b= new Boll1();
-		
+
+		JFrame f = new JFrame();
+		Boll1 b = new Boll1();
+
 		f.setContentPane(b);
 		f.setVisible(true);
 		f.pack();
-		
-		
-		
-		
+
 	}
-	
+
 }
